07be4dfbf46fac052ed3b5eb8cd9d8ffbf6003a5
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-29 00:13:33 +0530

    Update HolyFreezeCommand.java

--- src/main/java/me/priyam/holycontrol/commands/HolyFreezeCommand.java
+++ src/main/java/me/priyam/holycontrol/commands/HolyFreezeCommand.java
@@ -7,18 +7,29 @@ import org.bukkit.command.CommandSender;
 
 public class HolyFreezeCommand implements CommandExecutor {
 
+    private final HolyControl plugin;
+
+    public HolyFreezeCommand(HolyControl plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 1) return false;
 
         if (args[0].equalsIgnoreCase("on")) {
             HolyControl.FREEZE_ENABLED = true;
-            sender.sendMessage("All players frozen");
-        } else if (args[0].equalsIgnoreCase("off")) {
-            HolyControl.FREEZE_ENABLED = false;
-            sender.sendMessage("Freeze disabled");
+            plugin.getServer().broadcast(
+                    plugin.getConfigManager().getMessage("holyfreeze.enabled-message")
+            );
         }
 
+        if (args[0].equalsIgnoreCase("off")) {
+            HolyControl.FREEZE_ENABLED = false;
+            plugin.getServer().broadcast(
+                    plugin.getConfigManager().getMessage("holyfreeze.disabled-message")
+            );
+        }
         return true;
     }
 }
