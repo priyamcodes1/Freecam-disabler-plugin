7e582c5da5b204c057e51078c1b2cab9bf769b31
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-29 00:13:24 +0530

    Update PvPCommand.java

--- src/main/java/me/priyam/holycontrol/commands/PvPCommand.java
+++ src/main/java/me/priyam/holycontrol/commands/PvPCommand.java
@@ -7,18 +7,29 @@ import org.bukkit.command.CommandSender;
 
 public class PvPCommand implements CommandExecutor {
 
+    private final HolyControl plugin;
+
+    public PvPCommand(HolyControl plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 1) return false;
 
         if (args[0].equalsIgnoreCase("on")) {
             HolyControl.PVP_ENABLED = true;
-            sender.sendMessage("PvP enabled");
-        } else if (args[0].equalsIgnoreCase("off")) {
-            HolyControl.PVP_ENABLED = false;
-            sender.sendMessage("PvP disabled");
+            plugin.getServer().broadcast(
+                    plugin.getConfigManager().getMessage("pvp.enabled-message")
+            );
         }
 
+        if (args[0].equalsIgnoreCase("off")) {
+            HolyControl.PVP_ENABLED = false;
+            plugin.getServer().broadcast(
+                    plugin.getConfigManager().getMessage("pvp.disabled-message")
+            );
+        }
         return true;
     }
 }
