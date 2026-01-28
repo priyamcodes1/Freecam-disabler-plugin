8fedcd1ae5ecbace3a7f0717db64e9fa75a8bbe4
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-28 23:35:01 +0530

    Update FreezeListener.java

--- src/main/java/me/priyam/holycontrol/freeze/FreezeListener.java
+++ src/main/java/me/priyam/holycontrol/freeze/FreezeListener.java
@@ -1,11 +1,9 @@
 package me.priyam.holycontrol.freeze;
 
 import me.priyam.holycontrol.HolyControl;
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.*;
+import org.bukkit.event.player.*;
 import org.bukkit.event.block.*;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.Location;
 
 public class FreezeListener implements Listener {
@@ -13,14 +11,11 @@ public class FreezeListener implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
         if (!HolyControl.FREEZE_ENABLED) return;
-
-        Location from = e.getFrom();
-        Location to = e.getTo();
-
-        to.setX(from.getX());
-        to.setY(from.getY());
-        to.setZ(from.getZ());
-        e.setTo(to);
+        Location f = e.getFrom(), t = e.getTo();
+        t.setX(f.getX());
+        t.setY(f.getY());
+        t.setZ(f.getZ());
+        e.setTo(t);
     }
 
     @EventHandler public void onBreak(BlockBreakEvent e) {
