d2de0d0a241cd14329fec1155b353cff6603ffb8
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-28 23:49:07 +0530

    Update HolyControl.java

--- src/main/java/me/priyam/holycontrol/HolyControl.java
+++ src/main/java/me/priyam/holycontrol/HolyControl.java
@@ -1,40 +1,22 @@
 package me.priyam.holycontrol;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import me.priyam.holycontrol.config.ConfigManager;
 import me.priyam.holycontrol.freecam.FreecamDetector;
-import me.priyam.holycontrol.freeze.FreezeListener;
-import me.priyam.holycontrol.pvp.PvPListener;
-import me.priyam.holycontrol.commands.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HolyControl extends JavaPlugin {
+public class HolyControl extends JavaPlugin {
 
-    public static boolean PVP_ENABLED = true;
-    public static boolean FREEZE_ENABLED = false;
-    public static HolyControl INSTANCE;
+    private ConfigManager configManager;
 
     @Override
     public void onEnable() {
-        INSTANCE = this;
-        saveDefaultConfig();
-        ConfigManager.load(this);
+        this.configManager = new ConfigManager(this);
+        getServer().getPluginManager().registerEvents(
+                new FreecamDetector(this, configManager), this
+        );
+    }
 
-        // listeners
-        getServer().getPluginManager().registerEvents(new FreezeListener(), this);
-        getServer().getPluginManager().registerEvents(new PvPListener(), this);
-
-        // commands
-        getCommand("pvp").setExecutor(new PvPCommand());
-        getCommand("holyfreeze").setExecutor(new HolyFreezeCommand());
-        getCommand("holycontrol").setExecutor(new ReloadCommand());
-
-        ToggleTabCompleter tab = new ToggleTabCompleter();
-        getCommand("pvp").setTabCompleter(tab);
-        getCommand("holyfreeze").setTabCompleter(tab);
-
-        // freecam detector
-        if (ConfigManager.FREECAM_ENABLED) {
-            FreecamDetector.register(this);
-        }
+    public ConfigManager getConfigManager() {
+        return configManager;
     }
 }
