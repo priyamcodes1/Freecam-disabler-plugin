bc74d018dc1833cf8489227bf32e4b51aa206d14
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-28 23:49:37 +0530

    Update FreecamDetector.java

--- src/main/java/me/priyam/holycontrol/freecam/FreecamDetector.java
+++ src/main/java/me/priyam/holycontrol/freecam/FreecamDetector.java
@@ -1,50 +1,35 @@
 package me.priyam.holycontrol.freecam;
 
-import net.kyori.adventure.text.Component;
 import me.priyam.holycontrol.HolyControl;
 import me.priyam.holycontrol.config.ConfigManager;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class FreecamDetector {
+public class FreecamDetector implements Listener {
 
-    private static final String TEST_KEY = "freecam.toggle";
+    private final HolyControl plugin;
+    private final ConfigManager config;
 
-    public static void register(HolyControl plugin) {
-        ProtocolLibrary.getProtocolManager().addPacketListener(
-            new PacketAdapter(plugin, ListenerPriority.HIGHEST,
-                PacketType.Play.Server.SYSTEM_CHAT) {
+    public FreecamDetector(HolyControl plugin, ConfigManager config) {
+        this.plugin = plugin;
+        this.config = config;
+    }
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!config.isFreecamAllowed()) {
+            new BukkitRunnable() {
                 @Override
-                public void onPacketSending(PacketEvent event) {
-                    Player p = event.getPlayer();
-                    if (p.hasPermission(ConfigManager.BYPASS_PERMISSION)) return;
-
-                    Component c = event.getPacket().getChatComponents().read(0);
-                    if (c == null) return;
-
-                    if (c.toString().contains(TEST_KEY)) {
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                p.kick(Component.text(ConfigManager.KICK_MESSAGE));
-                            }
-                        }.runTask(plugin);
-                    }
+                public void run() {
+                    player.kick(Component.text(config.getFreecamKickMessage()));
                 }
-            }
-        );
-
-        plugin.getServer().getPluginManager().registerEvents(
-            new org.bukkit.event.Listener() {
-                @org.bukkit.event.EventHandler
-                public void onJoin(org.bukkit.event.player.PlayerJoinEvent e) {
-                    Player p = e.getPlayer();
-                    plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
-                        p.sendMessage(Component.translatable(TEST_KEY));
-                    }, ConfigManager.JOIN_DELAY);
-                }
-            }, plugin
-        );
+            }.runTaskLater(plugin, 1L);
+        }
     }
 }
