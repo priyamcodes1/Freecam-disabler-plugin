49883730ae67dbc06c4779a586b3b2c13ec64120
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-29 00:13:15 +0530

    Update ToggleTabCompleter.java

--- src/main/java/me/priyam/holycontrol/commands/ToggleTabCompleter.java
+++ src/main/java/me/priyam/holycontrol/commands/ToggleTabCompleter.java
@@ -1,11 +1,18 @@
 package me.priyam.holycontrol.commands;
 
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
 import java.util.List;
 
 public class ToggleTabCompleter implements TabCompleter {
+
     @Override
-    public List<String> onTabComplete(CommandSender s, Command c, String a, String[] args) {
-        return args.length == 1 ? List.of("on", "off") : List.of();
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return List.of("on", "off");
+        }
+        return List.of();
     }
 }
