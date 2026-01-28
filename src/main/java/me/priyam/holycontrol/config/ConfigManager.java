11dcb5a1e1b55d6166eb87a99d3208e4dbb0921a
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-29 00:12:31 +0530

    Update ConfigManager.java

--- src/main/java/me/priyam/holycontrol/config/ConfigManager.java
+++ src/main/java/me/priyam/holycontrol/config/ConfigManager.java
@@ -2,56 +2,25 @@ package me.priyam.holycontrol.config;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.util.List;
 
 public class ConfigManager {
 
     private final JavaPlugin plugin;
-    private File bypassFile;
-    private FileConfiguration bypassConfig;
+    private final MiniMessage mm = MiniMessage.miniMessage();
 
     public ConfigManager(JavaPlugin plugin) {
         this.plugin = plugin;
-
         plugin.saveDefaultConfig();
-        setupBypassFile();
-        reload();
     }
 
-    private void setupBypassFile() {
-        bypassFile = new File(plugin.getDataFolder(), "bypass.yml");
-        if (!bypassFile.exists()) {
-            plugin.saveResource("bypass.yml", false);
-        }
-        bypassConfig = YamlConfiguration.loadConfiguration(bypassFile);
+    public String getActorName() {
+        return plugin.getConfig().getString("messages.actor-name", "Console");
     }
 
-    public void reload() {
-        plugin.reloadConfig();
-        bypassConfig = YamlConfiguration.loadConfiguration(bypassFile);
-    }
-
-    // -------- FREECAM --------
-
-    public boolean isFreecamAllowed() {
-        return plugin.getConfig().getBoolean("freecam.allow", false);
-    }
-
-    public boolean isBypassed(String playerName) {
-        List<String> list = bypassConfig.getStringList("freecam-bypass");
-        return list.stream().anyMatch(name -> name.equalsIgnoreCase(playerName));
-    }
-
-    public Component getKickMessage() {
-        String msg = plugin.getConfig().getString(
-                "freecam.kick-message",
-                "<red>Freecam is not allowed.</red>"
-        );
-        return MiniMessage.miniMessage().deserialize(msg);
+    public Component getMessage(String path) {
+        String msg = plugin.getConfig().getString(path, "");
+        msg = msg.replace("<actor>", getActorName());
+        return mm.deserialize(msg);
     }
 }
