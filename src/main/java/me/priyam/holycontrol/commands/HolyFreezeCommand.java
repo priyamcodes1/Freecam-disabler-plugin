36d187c107c265201d20e585f799da5cb8051cf1
ItzGlecni <priyamdwivedi1605@gmail.com>
2026-01-28 23:52:57 +0530

    Update HolyFreezeCommand.java

--- src/main/java/me/priyam/holycontrol/commands/HolyFreezeCommand.java
+++ src/main/java/me/priyam/holycontrol/commands/HolyFreezeCommand.java
@@ -1,15 +1,24 @@
 package me.priyam.holycontrol.commands;
 
 import me.priyam.holycontrol.HolyControl;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class HolyFreezeCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
-        if (a.length != 1) return false;
-        HolyControl.FREEZE_ENABLED = a[0].equalsIgnoreCase("on");
-        s.sendMessage("Freeze is now " + (HolyControl.FREEZE_ENABLED ? "ON" : "OFF"));
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length != 1) return false;
+
+        if (args[0].equalsIgnoreCase("on")) {
+            HolyControl.FREEZE_ENABLED = true;
+            sender.sendMessage("All players frozen");
+        } else if (args[0].equalsIgnoreCase("off")) {
+            HolyControl.FREEZE_ENABLED = false;
+            sender.sendMessage("Freeze disabled");
+        }
+
         return true;
     }
 }
